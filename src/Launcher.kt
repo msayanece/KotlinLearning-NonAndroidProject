adad64a9e36e4bf37102cdad15f18ccaf82121f7
Sayan <msayanece@gmail.com>
2019-05-09 23:23:38 +0530

    Initial commit

---
+++ src/Launcher.kt
@@ -0,0 +1,24 @@
+
+//This contains main() method (args: Array<String> is optional in kotlin 1.3)
+
+//14 no video yet to be seen
+fun main(){
+    //object creation
+    val objTest = CreateObjectTest()
+   /* //0
+    objTest.property2 = "object created: property1 is => ${objTest.property1}"*/
+    /*//1
+    println(objTest.property2)*/
+    /*//2
+    objTest.objectFunctionWithDefaultArg()*/
+    /*//3
+    objTest.ifElseTest(2, 6)*/
+    /*//4
+    println(objTest.stringEquals("asd", "asd"))*/
+   /* //5 = null handling
+    val objTest2: CreateObjectTest = null       //error CreateObjectTest non-null type
+    val objTest2: CreateObjectTest? = null       //no error CreateObjectTest is nullable
+    val t1 = objTest2?.property1                     //no error
+    val t2 = objTest2!!.property1                     //no error*/
+
+}
